95de1297a54d88c2b735247a268b5134290bd99c
Thales Miguel <thales.miguel@gmail.com>
2019-03-06 13:46:10 -0300

    Create cards repository

---
+++ cards/lib/cards.ex
@@ -0,0 +1,31 @@
+defmodule Cards do
+  @moduledoc """
+  Documentation for Cards.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Cards.hello()
+      :world
+
+  """
+  def create_deck do
+    values = ["Ace", "Two", "Three", "For", "Five"]
+    suits = ["Spades", "Clubs", "Hearts", "Diamonds"]
+
+    for suit <- suits, value <- values do
+      "#{value} of #{suit}"
+    end
+  end
+
+  def shuffle(deck) do
+    Enum.shuffle(deck)
+  end
+
+  def contains?(deck, card) do
+    Enum.member?(deck, card)
+  end
+end
